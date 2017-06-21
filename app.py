7dfd4f82ca42de985b30fb9d12dcff76c327c910
GreywindGit <GreywindGit@users.noreply.github.com>
2017-06-21 15:52:29 +0200

    Login & register routes in server code

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import requests
 
 app = Flask(__name__)
@@ -8,6 +8,16 @@ app = Flask(__name__)
 def main_page():
     planet_data = requests.get('https://swapi.co/api/planets').json()
     return render_template('index.html', planet_data=planet_data)
+
+
+@app.route('/login', methods=['POST'])
+def do_login():
+    return redirect(request.referrer)
+
+
+@app.route('/register', methods=['POST'])
+def do_register():
+    return redirect(request.referrer)
 
 
 if __name__ == '__main__':
