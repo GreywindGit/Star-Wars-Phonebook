53226b4ecc546126c0cbce5d11ae9954d1e05f5d
GreywindGit <GreywindGit@users.noreply.github.com>
2017-06-22 10:13:22 +0200

    Endpoints for register/login/logout

--- app.py
+++ app.py
@@ -10,13 +10,28 @@ def main_page():
     return render_template('index.html', planet_data=planet_data)
 
 
+@app.route('/login', methods=['GET'])
+def show_login():
+    return render_template('login.html')
+
+
 @app.route('/login', methods=['POST'])
 def do_login():
     return redirect(request.referrer)
 
 
+@app.route('/register', methods=['GET'])
+def show_register():
+    return render_template('register.html')
+
+
 @app.route('/register', methods=['POST'])
 def do_register():
+    return redirect(request.referrer)
+
+
+@app.route('/logout')
+def do_logout():
     return redirect(request.referrer)
 
 
