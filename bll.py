acc1a5591e239afcdfa7e9b26b894b2287f48ca5
GreywindGit <GreywindGit@users.noreply.github.com>
2017-06-23 10:36:32 +0200

    Query for voted planets, remove user id query

--- bll.py
+++ bll.py
@@ -19,17 +19,18 @@ def register_user(username, password):
 def validate_user(username, password):
     sql_string = "SELECT ID FROM swuser WHERE username = %s AND password = %s;"
     sql_variables = (username, password)
-    user_id = dal.get_data_from_table(sql_string, sql_variables)
+    user_id = dal.get_data_from_table(sql_string, sql_variables)[0][0]
     return user_id
-
-
-def get_user_id(username):
-    sql_string = "SELECT id FROM swuser WHERE username = %s;"
-    sql_variable = (username, )
-    return dal.get_data_from_table(sql_string, sql_variable)[0][0]
 
 
 def insert_vote(planet_id, user_id, submission_time):
     sql_string = "INSERT INTO planet_votes (planet_id, user_id, submission_time) VALUES (%s, %s, %s);"
     sql_variables = (planet_id, user_id, submission_time)
     dal.modify_table(sql_string, sql_variables)
+
+
+def get_voted_planets():
+    sql_string = "SELECT planet_id, count(planet_id) AS votes FROM planet_votes\
+                 GROUP BY planet_id ORDER BY votes desc;"
+    result = dal.get_json_from_table(sql_string)
+    return result
